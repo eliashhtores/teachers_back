88c06d4a9bb78f6d77047b56fdd1f4a362769e7b
eliashhtores <eliashhtorres@gmail.com>
2023-10-19 13:56:15 -0500

    Sorting activities by start_time

--- routes/activity.js
+++ routes/activity.js
@@ -1,54 +1,42 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
 const app = express()
-const pool = require('../database/db')
+const pool = require("../database/db")
 
 app.use(express.json())
 
-const winston = require('winston')
+const winston = require("winston")
 
 app.use(express.json())
 
 const logConfiguration = {
-    'transports': [
+    transports: [
         new winston.transports.File({
-            filename: 'logs/app.log'
-        })
-    ]
+            filename: "logs/app.log",
+        }),
+    ],
 }
 
 const winstonLogger = winston.createLogger(logConfiguration)
 
 // Get one activity by id
-router.get('/:id', getActivityByID, async (req, res) => {
+router.get("/:id", getActivityByID, async (req, res) => {
     res.json(res.activity)
 })
 
 // Get activity by evaluation id
-router.get('/evaluation/:id', getActivitiesByEvaluationID, async (req, res) => {
+router.get("/evaluation/:id", getActivitiesByEvaluationID, async (req, res) => {
     res.json(res.activities)
 })
 
 // Create activity
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
     try {
-        const {
-            name,
-            school,
-            grade,
-            letter,
-            director_id,
-        } = req.body
+        const { name, school, grade, letter, director_id } = req.body
 
         const newActivity = await pool.query(
-            'INSERT INTO activity (name, school, grade, letter, director_id) VALUES (?, ?, ?, ?, ?)',
-            [
-                name,
-                school,
-                grade,
-                letter,
-                director_id,
-            ]
+            "INSERT INTO activity (name, school, grade, letter, director_id) VALUES (?, ?, ?, ?, ?)",
+            [name, school, grade, letter, director_id]
         )
         res.status(201).json(newActivity)
     } catch (error) {
@@ -59,24 +47,13 @@ router.post('/', async (req, res) => {
 })
 
 // Update activity
-router.patch('/:id', async (req, res) => {
+router.patch("/:id", async (req, res) => {
     const { id } = req.params
-    const {
-        name,
-        school,
-        grade,
-        letter
-    } = req.body
+    const { name, school, grade, letter } = req.body
     try {
         const updatedActivity = await pool.query(
-            'UPDATE activity SET name = ?, school = ?, grade = ?, letter = ? WHERE id = ?',
-            [
-                name,
-                school,
-                grade,
-                letter,
-                id,
-            ]
+            "UPDATE activity SET name = ?, school = ?, grade = ?, letter = ? WHERE id = ?",
+            [name, school, grade, letter, id]
         )
         res.json(updatedActivity)
     } catch (error) {
@@ -90,8 +67,8 @@ router.patch('/:id', async (req, res) => {
 async function getActivityByID(req, res, next) {
     try {
         const { id } = req.params
-        const activity = await pool.query('SELECT * FROM activity WHERE id = ?', [id])
-        if (activity[0].length === 0) return res.status(404).json({ message: 'Activity not found', status: 404 })
+        const activity = await pool.query("SELECT * FROM activity WHERE id = ?", [id])
+        if (activity[0].length === 0) return res.status(404).json({ message: "Activity not found", status: 404 })
 
         res.activity = activity[0][0]
         next()
@@ -105,8 +82,10 @@ async function getActivityByID(req, res, next) {
 async function getActivitiesByEvaluationID(req, res, next) {
     try {
         const id = req.params.id
-        const activities = await pool.query('SELECT * FROM activity WHERE evaluation_id = ?', [id])
-        if (activities[0].length === 0) return res.status(404).json({ message: 'No activities found', status: 404 })
+        const activities = await pool.query("SELECT * FROM activity WHERE evaluation_id = ? ORDER BY start_time ASC", [
+            id,
+        ])
+        if (activities[0].length === 0) return res.status(404).json({ message: "No activities found", status: 404 })
 
         res.activities = activities[0]
         next()
